bc0792646c131ca8a67aefdde716617147697478
Fernando Santos <fern@ndo.io>
2021-03-11 13:24:52 -0300

    Finalization: Move "Failed Transactions - no opt-in status" code into engrid-oc

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
 // import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 
+declare global {
+  interface Window {
+    DataCaptureID: any;
+    pageJson: any;
+  }
+}
+
 function getUserData() {
   let phone = App.getFieldValue("supporter.phoneNumber");
   let sms_message_opt_in = document.getElementById(
@@ -41,9 +48,7 @@ function postAjax(url: string, data: any, success: Function) {
         })
         .join("&");
 
-  var xhr = window.XMLHttpRequest
-    ? new XMLHttpRequest()
-    : new ActiveXObject("Microsoft.XMLHTTP");
+  var xhr = new XMLHttpRequest();
   xhr.open("POST", url);
   xhr.onreadystatechange = function () {
     if (
@@ -61,6 +66,88 @@ function postAjax(url: string, data: any, success: Function) {
   return xhr;
 }
 
+// Data Capture Code
+function dataCapture() {
+  {
+    const isEN = !!("pageJson" in window);
+    const hasError = !!document.querySelector(".en__errorHeader");
+    const dcId = "DataCaptureID" in window ? window.DataCaptureID : 74684;
+    const url = `https://takeaction.oceanconservancy.org/page/${dcId}/data/1`;
+    const optInCheck = document.querySelector(
+      "[name='supporter.questions.20087']"
+    ) as HTMLInputElement;
+    const form = document.querySelector("form.en__component") as HTMLFormElement;
+    const inIframe = () => {
+      try {
+        return window.self !== window.top;
+      } catch (e) {
+        return true;
+      }
+    };
+    const createIframe = (src: string) => {
+      let ifrm = document.createElement("iframe");
+      ifrm.setAttribute("src", src);
+      ifrm.style.width = "0px";
+      ifrm.style.height = "0px";
+      document.body.appendChild(ifrm);
+    };
+    const createField = (name: string, value: string) => {
+      let input = document.createElement("input");
+
+      input.setAttribute("type", "hidden");
+
+      input.setAttribute("name", name);
+
+      input.setAttribute("value", value);
+
+      //append to form element that you want .
+      form.appendChild(input);
+    };
+    // Is we're not in an EN page, get out
+    if (!isEN) {
+      return;
+    }
+    if (App.getPageType() == "DONATION") {
+      // Donation Page
+      if (hasError) {
+        // The Donation Code will be executed only if we got a backend error
+        console.log("Data Capture Donation", dcId);
+        const formData = new FormData(form);
+        if (optInCheck && optInCheck.checked) {
+          // The fields we want to send
+          const formFields = [
+            "supporter.firstName",
+            "supporter.lastName",
+            "supporter.emailAddress",
+            "supporter.questions.20087",
+          ];
+          const data = [...formData.entries()];
+          const asString =
+            "?" +
+            data
+              .filter((element) => formFields.includes(element[0]))
+              .map(
+                (x) => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1].toString())}`
+              )
+              .join("&");
+          console.log(url + asString);
+          createIframe(url + asString);
+        }
+      }
+    } else if (inIframe()) {
+      // We only execute the Data Capture Page if we're ALSO embedded as iFrame
+      console.log("Data Capture Iframe");
+      // Get data from URL and create the Fields
+      const urlParams = new URLSearchParams(window.location.search);
+      for (const [key, value] of urlParams.entries()) {
+        createField(key, value);
+      }
+      // Send the Form
+      form.submit();
+    }
+  };
+}
+
 
 const options: Options = {
   ModalDebug: true,
@@ -70,7 +157,7 @@ const options: Options = {
   CurrencySymbol: '$',
   CurrencySeparator: '.',
   MediaAttribution: true,
-  onLoad: () => console.log("Starter Theme Loaded"),
+  onLoad: () => { console.log("Starter Theme Loaded"); dataCapture(); },
   onResize: () => console.log("Starter Theme Window Resized"),
   onSubmit: () => {
     console.log('%c Upland / Mobilecommons Script', 'font-size: 30px; background-color: #000; color: #FF0');
