8c1936621587d8c89fcac50956d4c24ba0827aea
bryancasler <bryan.casler@gmail.com>
2021-04-11 21:44:36 -0400

    Finished remaining QA items

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
 // import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
-
+import "./scripts/main.js";
 declare global {
   interface Window {
     DataCaptureID: any;
@@ -157,6 +157,8 @@ const options: Options = {
   CurrencySymbol: '$',
   CurrencySeparator: '.',
   MediaAttribution: true,
+  SkipToMainContentLink: true,
+  SrcDefer: true,
   Debug: false,
   onLoad: () => { console.log("Starter Theme Loaded"); dataCapture(); },
   onResize: () => console.log("Starter Theme Window Resized"),
