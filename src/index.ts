60586d1e1b16418ec0e01162e27ed04a3ff85099
bryancasler <bryan.casler@gmail.com>
2021-04-13 10:05:22 -0400

    Moved NeverBounce styles and Scripts into the theme.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
 // import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 import "./scripts/main.js";
+
 declare global {
   interface Window {
     DataCaptureID: any;
@@ -147,7 +148,6 @@ function dataCapture() {
     }
   };
 }
-
 
 const options: Options = {
   ModalDebug: true,
