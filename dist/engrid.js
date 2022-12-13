dca5cf5ba57ec276e7776d2effc9f757d09475b8
Fernando Santos <fern@ndo.io>
2022-12-13 10:57:02 -0300

    New Message for Max Amount of 25000

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, December 12, 2022 @ 14:16:30 ET
+ *  Date: Tuesday, December 13, 2022 @ 08:52:25 ET
  *  By: fernando
  *  ENGrid styles: v0.13.19
  *  ENGrid scripts: v0.13.24
@@ -18633,7 +18633,7 @@ const options = {
   MinAmount: 5,
   MaxAmount: 25000,
   MinAmountMessage: "Amount must be at least $5 - Contact us for assistance",
-  MaxAmountMessage: "Amount must be less than $25,000 - Contact us for assistance",
+  MaxAmountMessage: "Contact us to give above $25,000 at 202-280-6283 or vision@oceanconservancy.org",
   MediaAttribution: true,
   SkipToMainContentLink: true,
   SrcDefer: true,
