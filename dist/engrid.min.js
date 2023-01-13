975bc38432ae3b2135de2463900822af7fec4e6a
bryancasler <bryan.casler@gmail.com>
2023-01-12 22:28:27 -0500

    More stylings for Apple Pay

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, January 12, 2023 @ 21:39:27 ET
+ *  Date: Thursday, January 12, 2023 @ 22:27:32 ET
  *  By: bryancasler
  *  ENGrid styles: v0.13.32
  *  ENGrid scripts: v0.13.32
