4e10c09d9d8aee54ae5c5dc3896730de56747642
bryancasler <bryan.casler@gmail.com>
2023-08-03 17:38:23 -0400

    Added theming for sub-theme coloring on the mobile wave divider

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, August 3, 2023 @ 13:28:33 ET
+ *  Date: Thursday, August 3, 2023 @ 17:37:12 ET
  *  By: bryancasler
  *  ENGrid styles: v0.14.13
  *  ENGrid scripts: v0.14.14
