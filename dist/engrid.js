1441bf1a7c702edd2ef32473ac6d09a77cc82550
bryancasler <bryan.casler@gmail.com>
2023-08-08 16:54:10 -0400

    Formatting for hidden opt-in with highlight
    https://www.bugherd.com/projects/352663/tasks/5

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, August 3, 2023 @ 17:37:12 ET
+ *  Date: Tuesday, August 8, 2023 @ 16:51:41 ET
  *  By: bryancasler
  *  ENGrid styles: v0.14.13
  *  ENGrid scripts: v0.14.14
